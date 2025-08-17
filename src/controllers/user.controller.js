e5c18271bb2cd4214836688a5f5899c317bf5930
TarunMundhra <lthamundhra5@gmail.com>
2025-08-17 16:24:58 +0530

    add user account management features: change password, get current user, update account, and update avatar/cover image

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -230,9 +230,180 @@ const refreshAccessToken = asyncHandler(async(req,res) =>{
         "Invaild access token 222")
     }
 })
+
+const changePassword = asyncHandler(async(req,res) =>{
+    const {oldPassword, newPassword} = req.body
+
+    if(!oldPassword || !newPassword){
+        throw new ApiError(400,"Old password and new password are required")
+    }
+
+    const user = await User.findById(req.user._id)
+
+    if(!user){
+        throw new ApiError(404,"User not found")
+    }
+
+    const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
+
+    if(!isPasswordCorrect){
+        throw new ApiError(401,"Old password is incorrect")
+    }
+
+    user.password = newPassword
+    await user.save({validateBeforeSave : true})
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200,{}, "Password changed successfully")
+    )
+})
+
+const getCurrentUser = asyncHandler(async(req,res) =>{
+    const user = await User.findById(req.user._id)
+    .select("-password -refreshToken")
+
+    if(!user){
+        throw new ApiError(404,"User not found")
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, user, "Current user fetched successfully")
+    )
+})
+
+const updateaccount = asyncHandler(async(req,res) =>{
+    const {fullName,username} = req.body
+
+    if(!fullName || !email){
+        throw new ApiError(400,"Full name and email are required")
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                email : email,
+                username: username
+            }
+        },
+        { new: true  } // to return the updated user and validate before saving
+    ).select("-password");
+
+    // if(!user){
+    //     throw new ApiError(404,"User not found")
+    // }
+
+    // user.email = email
+    // user.username = username.toLowerCase()
+
+    // handle avatar and cover image updates
+    // if(req.files?.avatar && req.files.avatar.length > 0) {
+    //     const avatarLocalPath = req.files.avatar[0].path;
+    //     user.avatar = (await uploadOnCloudnary(avatarLocalPath)).url;
+    // }
+
+    // if(req.files?.coverImage && req.files.coverImage.length > 0) {
+    //     const coverImageLocalPath = req.files.coverImage[0].path;
+    //     user.coverImage = (await uploadOnCloudnary(coverImageLocalPath)).url;
+    // }
+
+    // await user.save({validateBeforeSave: true});
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, user, "User account updated successfully")
+    )
+})
+
+const updateUserAvatar = asyncHandler(async(req,res) =>{
+    const user = await User.findById(req.user._id).select("-password ");
+
+    if(!user){
+        throw new ApiError(404,"User not found")
+    }
+
+    const avatarLocalPath = req.files?.avatar[0]?.path; //req.files is from multer
+
+    if(!avatarLocalPath){
+        throw new ApiError(400,"Avatar file is missing or not provided")
+    }
+
+    // user.avatar = (await uploadOnCloudnary(avatarLocalPath)).url;
+
+    // if(!user.avatar){
+    //     throw new ApiError(400,"Avatar file upload failed")
+    // }
+
+    const avatar = await uploadOnCloudnary(avatarLocalPath);
+    if(!avatar.url){    
+        throw new ApiError(400," New Avatar file upload failed")
+    }
+
+    user.avatar = avatar.url;
+    // Save the user with the new avatar not whole user object
+    // to avoid overwriting other fields like password or refreshToken
+    // no need to use set operator here as we are directly updating the avatar field
+    // and not the whole user object
+
+    await user.save({validateBeforeSave: true});
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, user, "User avatar updated successfully")
+    )
+})
+const updateUserCoverImage = asyncHandler(async(req,res) =>{
+    const user = await User.findById(req.user._id).select("-password ");
+
+    if(!user){
+        throw new ApiError(404,"User not found")
+    }
+
+    const avatarLocalPath = req.files?.avatar[0]?.path; //req.files is from multer
+
+    if(!coverImageLocalPath){
+        throw new ApiError(400,"coverImage file is missing or not provided")
+    }
+
+    // user.coverImage = (await uploadOnCloudnary(coverImageLocalPath)).url;
+
+    // if(!user.coverImage){
+    //     throw new ApiError(400,"coverImage file upload failed")
+    // }
+
+    const coverImage = await uploadOnCloudnary(coverImageLocalPath);
+    if(!coverImage.url){    
+        throw new ApiError(400," New coverImage file upload failed")
+    }
+
+    user.coverImage = coverImage.url;
+    // Save the user with the new coverImage not whole user object
+    // to avoid overwriting other fields like password or refreshToken
+    // no need to use set operator here as we are directly updating the coverImage field
+    // and not the whole user object
+
+    await user.save({validateBeforeSave: true});
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, user, "User coverImage updated successfully")
+    )
+})
 export {
     registerUser,
     loginUser,
     logoutUser,
-    refreshAccessToken
+    refreshAccessToken,
+    changePassword,
+    getCurrentUser,
+    updateaccount,
+    updateUserAvatar,
+    updateUserCoverImage
 }
